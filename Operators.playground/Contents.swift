1af8b96a9082702fcce0cdcc01046625cffe75aa
Evgeniy <127342275+Evgeniy-Muhamethin@users.noreply.github.com>
2023-05-24 16:44:44 +0300

    24.05.23
    New file was added. Operator was choice learned

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var _numberOne : Int8
+var _numberTwo : Int8
+
+_numberOne = 12
+_numberTwo = 14
+
+var _numberResult = _numberOne > _numberTwo ? _numberOne - _numberTwo : _numberOne + _numberTwo
+
+/*if _numberOne > _numberTwo
+{
+    _numberOne += 2
+}
+else
+{
+    _numberTwo += 3
+}*/
