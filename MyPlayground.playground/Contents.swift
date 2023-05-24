fafa52f24ab657472894a8dfd2429802b6c8592d
Evgeniy <127342275+Evgeniy-Muhamethin@users.noreply.github.com>
2023-05-24 16:23:05 +0300

    24.05.23
    Data type was learned. New file was added.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+let x = 8
+let z : Double
+z = 2
+let y = Double(x) / z
+
+let a = 10
+let b = 3.0
+let c = Double(a) / b
+
+let q = 15
+let w = 3.1
+let e = q / Int(w)
+
+var r : Int8 = 23
+var t : Int8 = 34
+var u : Int32 = Int32(r) + Int32(t)
